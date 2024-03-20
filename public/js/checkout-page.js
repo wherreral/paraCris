ef4f76faab73241572d466868a6876b07824bfc7
Cristhian Hernandez <cristhianjhl.web@gmail.com>
2024-03-19 20:11:02 -0500

    update checkout submit method

--- public/js/checkout-page.js
+++ public/js/checkout-page.js
@@ -97,13 +97,30 @@ function renderSummaryCartItems() {
 
 function handleCheckoutSubmit(event) {
 	event.preventDefault();
+	const shoppingCartItems = getShoppingCartListFromLocalStorage();
+	if (!Boolean(shoppingCartItems.length)) {
+		const message = document.createElement("small");
+		message.classList.add("checkout-page__message", "checkout-page__message-error");
+		message.textContent = "No hay productos para procesar.";
+		checkoutAddressForm.appendChild(message);
+		setTimeout(() => {
+			const message = document.querySelector(".checkout-page__message");
+			if (message) message.remove();
+		}, 4500);
+		return;
+	}
 	checkoutPageFormSubmitButton.disabled = true;
 	checkoutPageFormSubmitButton.textContent = "Procesando...";
 
 	const formData = new FormData(event.target);
 	const data = {
+		name: formData.get("name"),
+		lastname: formData.get("lastname"),
+		email: formData.get("email"),
+		phone: formData.get("phone"),
 		address: formData.get("address"),
-		city: formData.get("city"),
+		comune: formData.get("comune"),
+		region: formData.get("region"),
 		payment: formData.get("payment"),
 	};
 
